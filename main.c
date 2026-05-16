88841a3c43a8565a235ed062209b4a9aaeb80a1e
mazerissa <youcefelghoul@yahoo.com>
2026-05-16 23:48:10 +0200

    fixed some commands....ect

--- main.c
+++ main.c
@@ -1,70 +1,69 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <windows.h>
+#include <windows.h> //nik el windows
+#include <direct.h>
 
 int main() {
     char input[100];
-    bool keep_running = true;
+    char cwd[MAX_PATH];
+    bool running = true;
 
-    printf("Type 'help' for commands or 'exit' to quit.\n\n");
+    printf("CeaShell\n\n");
 
     do {
-        printf("ceashell> ");
+        if (GetCurrentDirectoryA(MAX_PATH, cwd)) {
+            printf("[%s]\nceashell> ", cwd);
+        } else {
+            printf("ceashell> ");
+        }
         fflush(stdout);
 
         if (fgets(input, 100, stdin) == NULL) break;
-
         input[strcspn(input, "\r\n")] = 0;
 
         char input_copy[100];
         strcpy(input_copy, input);
         char *command = strtok(input_copy, " ");
 
-        if (command == NULL || strlen(command) == 0) {
-            continue;
-        }
-
+        if (command == NULL || strlen(command) == 0) continue;
+        
         if (strcmp(command, "exit") == 0) {
-            keep_running = false;
+            running = false;
         } 
         else if (strcmp(command, "help") == 0) {
-            printf("--- CeaShell Help ---\n");
-            printf("exit       : Close the shell\n");
-            printf("help       : Show this menu\n");
-            printf("system     : Any other command will run as a Windows process\n");
-        } 
+            printf("Built-ins: exit, cd, clear\n");
+        }
+        else if (strcmp(command, "cd") == 0) {
+            char *path = strtok(NULL, "");
+            if (path == NULL) {
+                printf("Usage: cd <directory>\n");
+            } else {
+                if (!SetCurrentDirectoryA(path)) {
+                    printf("Directory not found: %s\n", path);
+                }
+            }
+        }
+        else if (strcmp(command, "clear") == 0) {
+            system("clear"); 
+        }
         else {
             STARTUPINFO si;
             PROCESS_INFORMATION pi;
-
             ZeroMemory(&si, sizeof(si));
             si.cb = sizeof(si);
             ZeroMemory(&pi, sizeof(pi));
 
-            if (CreateProcess(
-                    NULL,          
-                    input,          
-                    NULL,           
-                    NULL,           
-                    FALSE,          
-                    0,              
-                    NULL,           
-                    NULL,            
-                    &si,            
-                    &pi)            
-            ) {
+            if (CreateProcess(NULL, input, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
                 WaitForSingleObject(pi.hProcess, INFINITE);
-
                 CloseHandle(pi.hProcess);
                 CloseHandle(pi.hThread);
-            } 
-            else {
-                printf("CeaShell: '%s' is not recognized as an internal or external command. \n", command);
+            } else {
+                printf("CeaShell: Command '%s' failed car Tu es stupide.\n", command);
             }
         }
 
-    } while (keep_running);
+    } while (running);
 
     return 0;
 }
