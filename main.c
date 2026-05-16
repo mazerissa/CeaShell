45867d28f31fc67bbc534e2411fcec4f47c510c2
mazerissa <youcefelghoul@yahoo.com>
2026-05-16 23:22:03 +0200

    This is windows-only shell because their ass think, they're gods or something, I got to hate Microslop

--- main.c
+++ main.c
@@ -1,36 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <windows.h>
 
-int main(int argc, char *argv)
-{
+int main() {
     char input[100];
-    bool running = true;
+    bool keep_running = true;
 
-    do{
-    printf("CeaShell> ");
-    fflush(stdout);
-    fgets(input, sizeof(input), stdin);
-    
-    char *command = strtok(input, " \n");
-    
-    if (command == NULL) {
-        continue;
-    }
+    printf("Type 'help' for commands or 'exit' to quit.\n\n");
 
-    if (strcmp(command, "exit") == 0) {
-        running = false;
-    }
-    else if (strcmp(command, "help") == 0) {
-        printf("Available commands: exit, help\n");
-    }
-    else {
-        printf("CeaShell: '%s' is not a recognized command. \n", command);
-    }
-    }
+    do {
+        printf("ceashell> ");
+        fflush(stdout);
 
-    while(running);
-    printf("Tschuss \n");
+        if (fgets(input, 100, stdin) == NULL) break;
+
+        input[strcspn(input, "\r\n")] = 0;
+
+        char input_copy[100];
+        strcpy(input_copy, input);
+        char *command = strtok(input_copy, " ");
+
+        if (command == NULL || strlen(command) == 0) {
+            continue;
+        }
+
+        if (strcmp(command, "exit") == 0) {
+            keep_running = false;
+        } 
+        else if (strcmp(command, "help") == 0) {
+            printf("--- CeaShell Help ---\n");
+            printf("exit       : Close the shell\n");
+            printf("help       : Show this menu\n");
+            printf("system     : Any other command will run as a Windows process\n");
+        } 
+        else {
+            STARTUPINFO si;
+            PROCESS_INFORMATION pi;
+
+            ZeroMemory(&si, sizeof(si));
+            si.cb = sizeof(si);
+            ZeroMemory(&pi, sizeof(pi));
+
+            if (CreateProcess(
+                    NULL,          
+                    input,          
+                    NULL,           
+                    NULL,           
+                    FALSE,          
+                    0,              
+                    NULL,           
+                    NULL,            
+                    &si,            
+                    &pi)            
+            ) {
+                WaitForSingleObject(pi.hProcess, INFINITE);
+
+                CloseHandle(pi.hProcess);
+                CloseHandle(pi.hThread);
+            } 
+            else {
+                printf("CeaShell: '%s' is not recognized as an internal or external command. \n", command);
+            }
+        }
+
+    } while (keep_running);
 
     return 0;
 }
