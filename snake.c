b20995f283914f8c105918b60021910b83916f82
mazerissa <youcefelghoul@yahoo.com>
2026-05-22 22:51:26 +0200

    cross-platform support and emulations

--- snake.c
+++ snake.c
@@ -5,3 +5,30 @@
 
 #ifdef _WIN32
     #include <windows.h>
+    #include <conio.h>
+#else
+    #include <unistd.h>
+    #include <termios.h>
+    #include <sys/stat.h>
+
+    int _kbhit() {
+        struct timeval tv = { 0L, 0L };
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(0, &fds);
+        return select(1, &fds, NULL, NULL, &tv);
+    }
+
+    int _getch(void) {
+        struct termios oldattr, newattr;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldattr);
+        newattr = oldattr;
+        newattr.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+        return ch;
+    }
+
+#endif 
